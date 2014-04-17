984ca5e2e52ad0d7591a0ae3a9dcec5ea37b0c57
William Huang <wwhuang@umich.edu>
2014-04-16 22:10:20 -0400

    adjustments for new data packet

--- formatters/opoParser.py
+++ formatters/opoParser.py
@@ -20,34 +20,56 @@ class opoParser (parser.parser):
 			h = hex(raw_t)
 			t = int(h.split('x')[1])
 			return t
+
 		ret = {}
+		n = datetime.datetime.now()
 
 		# Opo-Specific
-		s = struct.unpack('!10s H I H H 5B I', data)
+		s = struct.unpack('!10s I I H H 8B H 8B', data)
 		gatd_profile_id   = s[0]
-		ret['tx_id']      = s[1]
-		ret['seq']        = s[2]
-		ret['last_tx_id'] = s[3]
-		ret['t_ul_rf']    = s[4]
-		ret['full_time']  = list(s[5:10])
-		ret['last_seq']   = s[10]
-		ret['range']      = float(ret['t_ul_rf'])/32000.0 * 340.29 - .12
-
+		ret['seq'] = s[1]
+		ret['reset_counter'] = s[2]
+		ret['m_id'] = s[3]
+		ret['last_tx_id'] = s[4]
+		ret['m_full_time'] = list(s[5:13])
+		ret['dt_ul_rf'] = s[13]
+		ret['last_full_time'] = list(s[14:22])
+		ret['range'] = float(ret['dt_ul_rf'])/32000.0 * 340.29 - .12
 
 		for i in range(len(ret['full_time'])):
 			"""
-			Time Format, from 0-5: second, minute, hour date, month. Year is assumed 2014
+			Time Format, from 0-8: second, minute, hour, day (wrong), date, month, year (no century)
 			"""
 			ret['full_time'][i] = convert_bcd(ret['full_time'][i])
+			ret['last_full_time'][i] = convert_bcd(ret['last_full_time'][i])
 
-		sec = ret['full_time'][0]
-		minute = ret['full_time'][1]
-		hr = ret['full_time'][2]
-		d = ret['full_time'][3]
-		month = ret['full_time'][4]
+		ret['full_time'][7] += 2000 # accounting for century bits in month
 
-		m_date = datetime.datetime(2014, month, d, hr, minute, sec, tzinfo=pytz.timezone('US/Eastern'))
-		ret['full_time'] = time.mktime(m_date.timetuple())
+		true_full_time = [0, n.second, n.minute, n.hour, n.weekday(), n.day, n.month, n.year]
+		ret['true_full_time'] = true_full_time
+
+		sec = ret['full_time'][1]
+		minute = ret['full_time'][2]
+		hr = ret['full_time']3]
+		d = ret['full_time'][5]
+		month = ret['full_time'][6]
+		year = ret['full_time'][7]
+
+		lsec = ret['last_full_time'][1]
+		lminute = ret['last_full_time'][2]
+		lhr = ret['last_full_time']3]
+		ld = ret['last_full_time'][5]
+		lmonth = ret['last_full_time'][6]
+		lyear = ret['last_full_time'][7]
+
+		m_date = datetime.datetime(year, month, d, hr, minute, sec, tzinfo=pytz.timezone('US/Eastern'))
+		l_date = datetime.datetime(lyear, lmonth, ld, lhr, lminute, lsec, tzinfo=pytz.timezone('US/Eastern'))
+
+		tdiff = n - m_date
+
+		adjusted_l_date = l_date + tdiff
+
+		ret['adjusted_last_full_timestamp'] = time.mktime(adjusted_l_date.timetuple())
 
 		# Standard GATD Footer
 		ret['address'] = str(meta['addr'])

700321abdf19b6a7a08839f1db00151200045ac3
Branden Ghena <brghena@umich.edu>
2014-06-14 18:43:40 -0400

    Created a parser for MAC Address scans

---
+++ formatters/macAddrParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class fitbitLocatorParser (parser.parser):
+
+	name = 'MAC Address Parser'
+	description = 'Scans of MAC Addresses of all wi-fi devices in a location.'
+	no_parse = True
+
+	def __init__ (self):
+		pass

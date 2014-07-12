ea3a805971e77b8c0246aff463a012d738bc5a71
Brad Campbell <bradjc5@gmail.com>
2014-07-12 16:42:47 -0400

    breeze

--- formatters/airflowParser.py
+++ formatters/airflowParser.py
@@ -27,6 +27,19 @@ class airflowParser ():
 				ret['airflow_state'] = 'off'
 			ret['seq_no'] = values[2]
 
+		elif airflow_type == 1:
+			values = struct.unpack("!BLLH5H", data)
+			ret['type']    = 'breeze'
+			ret['version'] = values[0]
+			ret['seq_no']  = values[1]
+			ret['total']   = values[2]
+			ret['count']   = values[3]
+			ret['airflow_reading'] = float(ret['total']/ret['count'])
+			# if ret['airflow_reading'] > 7000:
+			# 	ret['airflow_state'] = 'on'
+			# else:
+			# 	ret['airflow_state'] = 'off'
+
 		ret['address'] = str(meta['addr'])
 		ret['port']    = meta['port']
 		ret['time']    = meta['time']

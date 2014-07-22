852f168ec26b96f3cc2a2f1c7a0eef4358d44ab0
mclarkk <meghan.leah.clark@gmail.com>
2014-07-22 19:52:10 -0400

    first commit of ninjaBlocksRFParser, which handles all PIR motion, door, and window events from the Ninja Block system.

---
+++ formatters/ninjaBlocksRFParser.py
@@ -0,0 +1,11 @@
+import parser
+
+class ninjaBlocksRFParser (parser.parser):
+
+	# Parameters for this profile
+	name = 'Ninja Blocks RF'
+	description = 'Ninja Blocks RF Data - PIR Motion, Doors, Windows.'
+	no_parse = True
+
+	def __init__ (self):
+		pass

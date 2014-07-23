cfb780f4260dfba2fce98b7042183998d6b86c12
mclarkk <meghan.leah.clark@gmail.com>
2014-07-22 23:29:06 -0400

    first commit of indoor and outdoor temperature/humidity parsers for Ninja Blocks and the Weather Underground API respectively.

---
+++ formatters/ninjaBlocksClimateParser.py
@@ -0,0 +1,11 @@
+import parser
+
+class ninjaBlocksClimateParser (parser):
+
+	# Parameters for this profile
+	name = 'Ninja Blocks Climate'
+	description = 'Ninja Blocks Temperature and Humidity Data'
+	no_parse = True
+
+	def __init__ (self):
+		pass
---
+++ formatters/wundergroundClimateParser.py
@@ -0,0 +1,11 @@
+import parser
+
+class wundergroundClimateParser (parser):
+
+	# Parameters for this profile
+	name = 'Weather Underground'
+	description = 'Weather data from the Weather Underground.'
+	no_parse = True
+
+	def __init__ (self):
+		pass

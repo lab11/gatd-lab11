1dfefbed27ac1cf6de325eee39eb49a5f63c0dac
Branden Ghena <brghena@umich.edu>
2014-09-05 14:20:01 -0400

    Added lightControlParser

---
+++ formatters/lightControlParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class lightControlParser (parser.parser):
+
+	name = 'Light Control Parser'
+	description = 'Send commands to lights in certain locations'
+	no_parse = True
+
+	def __init__ (self):
+		pass

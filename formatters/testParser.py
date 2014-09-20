26231f43891ce0f6ce9e151f2bd4a0ad1fb83d72
Branden Ghena <brghena@umich.edu>
2014-09-20 16:38:38 -0400

    Added parser for testParser, used for debugging

---
+++ formatters/testParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class testParser (parser.parser):
+
+	name = 'Testing'
+	description = 'Parser for use while testing and debugging'
+	no_parse = True
+
+	def __init__ (self):
+		pass

2ef62b88d5f9eee24b661870833aa4e2a58381f8
Pat Pannuto <pat.pannuto@gmail.com>
2014-09-29 13:25:36 -0400

    Add m3NearfieldParser

---
+++ formatters/m3NearfieldParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class m3NearfieldParser (parser.parser):
+
+	name = 'm3 Nearfield Radio'
+	description = 'Michigan Micro Mote bridge parser.'
+	no_parse = True
+
+	def __init__ (self):
+		pass

fbf01dafc3a7a08fc42853e26301714217f61247
Branden Ghena <brghena@umich.edu>
2014-10-11 18:22:06 -0400

    Created new bleAddrParser

---
+++ formatters/bleAddrParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class bleAddrParser (parser.parser):
+
+	name = 'BLE Address Parser'
+	description = 'Scans of BLE Addresses of all bluetooth low-energy devices in a location.'
+	no_parse = True
+
+	def __init__ (self):
+		pass

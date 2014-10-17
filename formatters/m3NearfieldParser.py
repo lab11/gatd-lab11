6ef44e768c4b7182cba3a99bd813441609c46fcc
Pat Pannuto <pat.pannuto@gmail.com>
2014-10-17 15:32:25 -0400

    Parse m3 now

--- formatters/m3NearfieldParser.py
+++ formatters/m3NearfieldParser.py
@@ -4,7 +4,17 @@ class m3NearfieldParser (parser.parser):
 
 	name = 'm3 Nearfield Radio'
 	description = 'Michigan Micro Mote bridge parser.'
-	no_parse = True
+	#no_parse = True
 
 	def __init__ (self):
 		pass
+
+	def parse (self, data, meta, extra, settings):
+		ret = {}
+
+		# Opo-Specific
+		s = struct.unpack('!10s 28B', data)
+		gatd_profile_id   = s[0]
+		ret['bytes'] = s[1:29]
+
+		return ret

03ca3b8b0539293e811c3eeae2bbc89e1df47303
Branden Ghena <brghena@umich.edu>
2014-10-16 23:51:48 -0400

    Updates to parser to work with terraswarm demo

--- formatters/bleAddrParser.py
+++ formatters/bleAddrParser.py
@@ -1,10 +1,44 @@
 import parser
+import json
 
-class bleAddrParser (parser.parser):
+class bleAddrParser ():
 
 	name = 'BLE Address Parser'
 	description = 'Scans of BLE Addresses of all bluetooth low-energy devices in a location.'
-	no_parse = True
+
+        scanner_mapping = {
+                #'mac address': 'some location in Berkeley'
+                'aa:bb:cc:dd:ee:ff': 'Room 1'
+                }
+
+        superhero_mapping = {
+                #'ble address': 'a superhero name'
+                }
 
 	def __init__ (self):
 		pass
+
+        def parse (self, data, meta, extra, settings):
+            ret = json.loads(json.loads(data[10:])['data'])
+
+            # do parsing for terraSwarm demo
+            if 'location_str' in ret and ret['location_str'] == 'terraswarm-demo':
+                # find the location string
+                if 'bbb_mac_addr' in ret and ret['bbb_mac_addr'] in self.scanner_mapping:
+                    ret['location_str'] = (
+                        'USA|California|Berkeley|UCB|Clark Kerr|Conference Center|' +
+                        self.scanner_mapping[ret['bbb_mac_addr']])
+                else:
+                    ret['location_str'] = 'unknown'
+
+                # add in a superhero identity
+                if 'ble_addr' in ret and ret['ble_addr'] in self.superhero_mapping:
+                    ret['identity'] = self.superhero_mapping[ret['ble_addr']]
+
+            # add meta data to the dict
+            ret['address'] = str(meta['addr'])
+            ret['port']    = meta['port']
+            ret['time']    = meta['time']
+
+            return ret
+            

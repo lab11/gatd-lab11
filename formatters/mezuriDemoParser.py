db7535e5c96ed4e74c40d3bb4151cf551379c9a1
Noah Klugman <nklugman@0587362258.wireless.umich.net>
2014-11-05 15:53:57 -0500

    adding in mezuriDemoParser

---
+++ formatters/mezuriDemoParser.py
@@ -0,0 +1,12 @@
+import parser
+
+class mezuriDemoParser (parser.parser):
+
+    # identifies people and their locations
+    name = 'mezuriDemo'
+    description = 'Global power state for the mezuri techcon demo.'
+    no_parse = True
+
+    def __init__ (self):
+        pass
+

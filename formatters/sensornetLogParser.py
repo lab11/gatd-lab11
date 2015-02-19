99ec47044ba26709bf00bb2093a581001136cc99
Thomas Zachariah <tzachari@umich.edu>
2015-02-19 14:56:59 -0500

    Adding SensorNet Logger Formatter

---
+++ formatters/sensornetLogParser.py
@@ -0,0 +1,10 @@
+import parser
+
+class testParser (parser.parser):
+
+	name = 'SensorNet Logger'
+	description = 'Parser for the SensorNet Logging (initially for surveys)'
+	no_parse = True
+
+	def __init__ (self):
+		pass
